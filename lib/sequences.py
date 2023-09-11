0f75ababc7705d4ee954c9d3663481cc5686ce36
Krystle Lee <l33.krys@gmail.com>
2023-09-11 14:35:47 -0700

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    arr = []
+    if length == 0:
+        arr
+    elif length ==1:
+        arr.append(0)
+    elif length >= 2:
+        i = 0
+        arr = [0, 1]
+        while len(arr) < length:
+            total = arr[i] + arr[i + 1]
+            arr.append(total)
+            i += 1
+    print(arr)
+        
+
+
+
+  
